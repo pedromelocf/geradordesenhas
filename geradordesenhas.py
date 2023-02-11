92f8bc0bcb7b7b3b5417c26897b6891fad40c62a
Pedro Melo <pedromelocf@yahoo.com>
2023-02-11 16:37:10 -0300

    Criando função para receber comprimento da senha via input do usuário. A entrada do usuário precisa ser maior que 6 e menor que 60, além de que precisa ser um inteiro.

--- geradordesenhas.py
+++ geradordesenhas.py
@@ -1,6 +1,20 @@
 import random
-import script
-
-
+import string
 
 print("Bem vindo ao gerador de senhas do Pedro!")
+
+def receber_comprimento():
+    while True:
+        comprimento = input("Escreva o comprimento de sua senha:")
+        try:
+            x = int(comprimento)
+            if x >=6 and x <=60:
+                print("Sua senha terá {} caracteres! Gerando senha...".format(comprimento))
+                break
+            else:
+                print("Valor não pode ser abaixo de '6' ou maior que '60', tente de novo")
+        except ValueError:
+            print("Precisa ser um número, tente de novo")
+    return comprimento
+
+receber_comprimento()
